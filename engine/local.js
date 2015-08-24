7ba2c31994435ecb041d5ac40a5ff5a3bcc14dae
fanjingang <fanjingang@diligrp.com>
2015-08-24 18:03:26 +0800

    Anonymous cominggit st

--- engine/local.js
+++ engine/local.js
@@ -1,42 +1,81 @@
 var define, require, local;
 (function(global){
-  var deps = [], config = {};
-  var root = document, head = root.head;
+  var deps = [], config = {},
+
+  var root = document,
+      head = root.head;
+
   var shift = Array.prototype.shift;
 
-  function creatNode(){
-    var node = root.creatElement("script");
-    node.type = "text/javascript";
-    node.charset = "utf-8";
-    node.async = "async";
+  var op = Object.prototype,
+      tostring = op.toString;
 
-    return node;
+  var regPro = /^([http|https]\:\/\/)/,
+      regUrl = /[\d\w\-\_]+\//g;
+
+  var scriptCreater = {
+    load: function(url){
+      this.node = this.create();
+      this.url = url;
+      this.append().watch();
+    },
+    create: function(){
+      var node = root.createElement("script");
+      node.type = "text/javascript";
+      node.charset = "utf-8";
+      node.async = "async";
+
+      return node;
+    },
+    watch: function(){
+      var _this = this, node = this.node;
+
+      node.addEventListener("load", function(){}, false);
+      node.addEventListener("error", function(){err(_this.url +" is't exsit.")}, false);
+
+      return this;
+    },
+    append: function(){
+      var node = this.node;
+      node.src = this.url;
+      head.appendChild(node);
+
+      return this;
+    }
   };
 
-  function loadNode(url){
-    var node = creatNode();
-    node.src = url;
-    head.appendChild(node);
-
-    return watchNode(node);
+  function scriptNode(url){
+    scriptCreater.load(url);
   };
 
-  function watchNode(node){
-    if (typeof node == "object"){
-      node.addEventListener("onload", function(){alert("loaded.")}, false);
+  function isType(type){
+    return function(x){
+      return tostring.call(x) == "[object "+ type +"]";
     };
   };
 
-  function isAry(x){
-    return null;
-  }
+  var isAry = isType("Array");
 
-  function each(callback){
-
+  function eachAry(ary, fn){
+    for (var i = 0, l = ary.length;i < l;i++){fn(ary[i], i)};
   };
 
   function err(word){
     throw new Error(word);
   };
+
+  function anonyer(deps, fns){
+    if (!isAry(deps)){
+
+    }
+  };
+
+  /* Get first script's config location from custom name local */
+  eachAry(root.scripts, function(elem, i){
+    if (elem.getAttribute("local")){
+      var local = elem.getAttribute("local");
+      scriptNode(local);
+    }
+  });
 
 })(this);
