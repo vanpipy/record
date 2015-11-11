4fefe828c1e57618ee01b3d4229323a5b8ff75e7
vanpipy <vanpipy@gmail.com>
2015-11-11 08:01:57 +0800

    Modify barm regexp && set new loop style.

--- barm/barm.js
+++ barm/barm.js
@@ -6,24 +6,65 @@
  * author by van
  */
 
-(function(global, factory){
-  global.barm = new factory();
-})(window, function(){
+(function(global, factory) {
+  global.barm = factory();
+})(window, function() {
+  var root = document;
 
-  var barm = function(selector){
+  var barm = function(selector) {
     return new barm.fn.select(selector);
   };
 
   barm.fn = barm.prototype = {
-    select: function(selector){
-      var s = [];
-      selector.replace(/([\s>~]?)([.#]?)([\d\w-]+)/g, function(all, relation, attrType, name){
-        s.push([relation, attrType, name]);
+    select: function(selector) {
+      var box = []; 
+      var nodes = [];
+      var start = root;
+      var i = 0;
+      var cache;
+
+      selector.replace(/([\>\-\+\~]*)(\s*[.#]?)([\d\w-]+)/g, function(match, rela, type, name, length, all) {
+        box.push([rela, type, name]);
         return '';
       });
 
-      return s;
+      var board = children(start);
+      var deep;
+
+      while (start.length - i) {
+        console.log(i, start[i]);
+        i++;
+
+        /*
+        if (start.length == i) {
+          i = 0;
+          deep = children(start.shift());
+
+          while (deep.length) {
+
+          }
+        };
+        */
+      };
+
+      return box;
     }
+  };
+
+  function children(parentNode) {
+    var nodes = parentNode.children;
+
+    return covert2Ary(nodes);
+  };
+
+  function covert2Ary(collection) {
+    var ary = [];
+
+    for ( var i = 0, l = collection.length; i < l; i++ ) {
+      ary[i] = collection[i];
+    };
+
+    return ary;
   };
 
   return barm;
