0e97a9b55e13f3d26d93ba8d4ccf7f29704782ab
vanpipy <vanpipy@gmail.com>
2016-01-21 19:06:48 -0500

    Modify lterator function.

--- barm/lterator.js
+++ barm/lterator.js
@@ -9,9 +9,9 @@
   var op = Object.prototype;
   var tostring = op.toString;
   var hasOwn = op.hasOwnProperty; 
-  var isFun = is('function');
-  var isAry = is('array');
-  var isObj = is('object');
+  var isFun = is('Function');
+  var isAry = is('Array');
+  var isObj = is('Object');
 
   function is (type) {
     return function (o) {
@@ -20,25 +20,46 @@
   };
 
   function lterator (data) {
-    this.index = [];
+    this.keys = [];
     this.data = data;
     this.pointer = 0;
 
-    this.init();
+    this.setLength();
   };
 
-  lterator.prototype.init = function () {
-    for (var i = 0, l = this.data.length; i < l; i++) {
-      this.index[i] = i;
+  lterator.prototype.setLength = function () {
+    this.length = 0;
+
+    this.setKeys(this.data);
+  };
+
+  lterator.prototype.setKeys = function (input) {
+    if (isAry(input)) {
+      this.length = input.length;
+
+      for (var i = 0, l = this.length; i < l; i++) {
+        this.keys[i] = i;
+      };
     };
+
+    if (isObj(input)) {
+      for (var each in input) {
+        this.keys.push(each);
+      };
+
+      this.length = this.keys.length;
+    };
+
+    return this.length;
   };
 
   lterator.prototype.current = function () {
-    return this.data[ this.index[this.pointer] ];
+    return this.data[ this.keys[this.pointer] ];
   };
 
   lterator.prototype.next = function () {
     this.pointer++;
+    return this.current();
   };
 
   window.lterator = lterator;
@@ -46,9 +67,10 @@
 })(window);
 
 var a = ['a', 'b', 'c'];
+var c = {'a': 'abc', 'b': 2, 'c': 3};
 
-var b = new lterator(a);
+var b = new lterator(c);
 
-b.current();
-b.next();
-b.current();
+console.log(b.current());
+console.log(b.next());
+console.log(b.next());
